3d74a1df48528032f0ecfc0ff1677e5a54a0255b
Rustam <rustam.mamedov230@gmail.com>
2020-11-21 18:35:25 -0500

    update constants.py

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+#Adjustable constants
+
+scrollrate = 20
+diagonal_x_sens = 45 #magnitude of change to move nose horizontally to move diagonally
+diagonal_y_sens = 20 #magnitude of change to move nose vertically to move diagonally
+horizontal_x_sens = 45 #magnitude of change to move nose horizontally to move horizontally
+vertical_y_sens = 20 #magnitude of change to move nose vertically to move vertically
