3d065518c04ac92d5c58c2b51c059fae7c285c3c
Ari Wasch <34742370+ariwasch@users.noreply.github.com>
2020-11-21 23:02:56 -0500

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,9 @@
 #Adjustable constants
 
-scrollrate = 20
-horizontal_x_sens = 50 #magnitude of change to move nose horizontally to move horizontally
-vertical_y_sens = 25 #magnitude of change to move nose vertically to move vertically
+SCROLLRATE = 80
+MOUSE_MOVEMENT = 20
+HORIZONTAL_X_SENS = 50 #magnitude of change to move nose horizontally to move horizontally
+VERTICAL_Y_SENS = 17 #magnitude of change to move nose vertically to move vertically
 CLICK_THRESHOLD = 10 #Minimum pixel gap between top lip and bottom lip to trigger a click
 ALIGNED_THRESHOLD = 30 #Maximum difference in sizes of each side of face to be aligned with camera
 SCROLL_UP_THRESHOLD = 30 #Maximum tilt to the right before scrolling up
